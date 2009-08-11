6077612726f3890ad64926bd1742191b0ca2f393
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2009-08-11 08:57:15 +0000

    qrepl improvements

---
+++ demo/mouseevents.R
@@ -0,0 +1,44 @@
+
+## a basic demo of mouse event callbacks
+
+library(qtutils)
+library(qtbase)
+library(qtpaint)
+library(mosaiq)
+
+
+
+tsplot <- function(y, x = NULL)
+{
+    if (missing(x))
+        x <-
+            if (is(x, "ts")) times(y)
+            else seq_along(y)
+    render_data<- function(item, painter, exposed) {
+        qantialias(painter) <- FALSE
+        mosaiq.points(x = x, y = y, type = "l", painter = painter)
+    }
+    handle_wheel <- function(event)
+    {
+        ## print(event$delta)
+        lims <- qlimits(points)
+        llist <- list(xlim = lims[, 1], ylim = lims[, 2])
+        f <- -0.01 * event$delta / 120
+        qlimits(points) <-
+            qrect(extendrange(llist$xlim, f = f),
+                  llist$ylim)
+        qupdate(points)
+    }
+    scene <- qgraphicsScene()
+    root <- qlayer(scene)
+    points <-
+        qlayer(root,
+               paintFun = render_data,
+               wheelFun = handle_wheel)
+    qlimits(points) <- qrect(range(x), range(y))
+    view <- qplotView(scene = scene, opengl = FALSE)
+    qsetContextMenuPolicy(view, "actions")
+    qsetDeleteOnClose(view, TRUE)
+    view
+}
+
